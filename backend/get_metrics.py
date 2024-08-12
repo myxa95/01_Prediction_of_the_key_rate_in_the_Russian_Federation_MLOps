c5053dbfdd63f93c64b974ddbb95c225824d4bab
myxa95 <myxa-2005@mail.ru>
2024-08-13 01:46:20 +0300

    first commit

---
+++ backend/get_metrics.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, mean_squared_error
+
+def r2_adjusted(y_true: np.ndarray, y_pred: np.ndarray,
+                X_test: np.ndarray) -> float:
+    """Коэффициент детерминации (множественная регрессия)"""
+    N_objects = len(y_true)
+    N_features = X_test.shape[1]
+    r2 = r2_score(y_true, y_pred)
+    return 1 - (1 - r2) * (N_objects - 1) / (N_objects - N_features - 1)
+
+
+def mpe(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """Mean percentage error"""
+    return np.mean((y_true - y_pred) / y_true) * 100
+
+
+def mape(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """Mean absolute percentage error"""
+    return np.mean(np.abs((y_pred - y_true) / y_true)) * 100
+
+
+def wape(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """Weighted Absolute Percent Error"""
+    return np.sum(np.abs(y_pred - y_true)) / np.sum(y_true) * 100
+
+
+def huber_loss(y_true: np.ndarray, y_pred: np.ndarray, delta: float = 1.345):
+    """Функция ошибки Хьюбера"""
+    assert len(y_true) == len(y_pred), 'Разные размеры данных'
+    huber_sum = 0
+    for i in range(len(y_true)):
+        if abs(y_true[i] - y_pred[i]) <= delta:
+            huber_sum += 0.5 * (y_true[i] - y_pred[i])**2
+        else:
+            huber_sum += delta * (abs(y_true[i] - y_pred[i]) - 0.5 * delta)
+    huber_sum /= len(y_true)
+    return huber_sum
+
+
+def logcosh(y_true: np.ndarray, y_pred: np.ndarray):
+    """функция ошибки Лог-Кош"""
+    return np.sum(np.log(np.cosh(y_true - y_pred)))
+
+
+def rmsle(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
+    """
+    The Root Mean Squared Log Error (RMSLE) metric 
+    Логаритмическая ошибка средней квадратичной ошибки
+    """
+    try:
+        return np.sqrt(mean_squared_log_error(y_true, y_pred))
+    except:
+        return None
+
+def get_metrics(y_test: np.ndarray,
+                y_pred: np.ndarray,
+                name: str = None,):
+    """Генерация таблицы с метриками для задачи регрессии"""
+    df_metrics = pd.DataFrame()
+
+    df_metrics['model'] = [name]
+
+    df_metrics['MAE'] = mean_absolute_error(y_test, y_pred)
+    df_metrics['MAPE_%'] = mean_absolute_percentage_error(y_test, y_pred)
+    df_metrics['MSE'] = mean_squared_error(y_test, y_pred)
+    df_metrics['RMSE'] = np.sqrt(mean_squared_error(y_test, y_pred))
+
+
+    return df_metrics
